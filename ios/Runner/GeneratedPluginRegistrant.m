05bf26f664449712217e87fcc4a8e810cc64a19f
adeelafzal <adeelcheema990@gmail.com>
2020-08-24 11:45:00 +0500

    all updated final project

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flashy_tab_bar/FlashyTabBarPlugin.h>)
+#import <flashy_tab_bar/FlashyTabBarPlugin.h>
+#else
+@import flashy_tab_bar;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlashyTabBarPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlashyTabBarPlugin"]];
+}
+
+@end
